904f076b63581a339e5e0e389d462437a2b08d58
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2026-05-02 19:23:30 +0200

    feat: update scheme registration for IAM types
    - Rename apis/template.go to apis/dip.go
    - Register IAM v1alpha1 types in scheme
    - Remove sample/v1alpha1 reference

--- apis/dip.go
+++ apis/dip.go
@@ -14,28 +14,27 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package apis contains Kubernetes API for the Template provider.
+// Package apis contains Kubernetes API for the DIP provider.
 package apis
 
 import (
 	"k8s.io/apimachinery/pkg/runtime"
 
-	samplev1alpha1 "github.com/crossplane/provider-template/apis/sample/v1alpha1"
-	templatev1alpha1 "github.com/crossplane/provider-template/apis/v1alpha1"
+	iamv1alpha1 "github.com/crossplane/provider-template/apis/iam/v1alpha1"
+	dipv1alpha1 "github.com/crossplane/provider-template/apis/v1alpha1"
 )
 
 func init() {
-	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes,
-		templatev1alpha1.SchemeBuilder.AddToScheme,
-		samplev1alpha1.SchemeBuilder.AddToScheme,
+		dipv1alpha1.SchemeBuilder.AddToScheme,
+		iamv1alpha1.SchemeBuilder.AddToScheme,
 	)
 }
 
-// AddToSchemes may be used to add all resources defined in the project to a Scheme
+// AddToSchemes may be used to add all resources defined in the project to a Scheme.
 var AddToSchemes runtime.SchemeBuilder
 
-// AddToScheme adds all Resources to the Scheme
+// AddToScheme adds all Resources to the Scheme.
 func AddToScheme(s *runtime.Scheme) error {
 	return AddToSchemes.AddToScheme(s)
 }
